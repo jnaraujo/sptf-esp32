167c3949d988770f4a0c4822451625416859c8f9
Jônatas A. <hello@jnaraujo.com>
2025-12-24 13:37:39 -0300

    style: improve code formatting and consistency in SpotifyClient class and README

--- lib/spotify/spotify.h
+++ lib/spotify/spotify.h
@@ -1,39 +1,37 @@
 #pragma once
 
-#include <HTTPClient.h>
 #include <Arduino.h>
 #include <ArduinoJson.h>
+#include <HTTPClient.h>
 #include <base64.h>
 #include <debug.h>
 
 struct PlaybackState {
-  String title;
-  String artist;
-  String album;
-  bool isPlaying;
-  int volume_percent;
-  int progress_ms;
-  int duration_ms;
+    String title;
+    String artist;
+    String album;
+    bool isPlaying;
+    int volume_percent;
+    int progress_ms;
+    int duration_ms;
 
-  PlaybackState() {
-    title = "N/A";
-    artist = "N/A";
-    album = "N/A";
-    isPlaying = false;
-    volume_percent = 0;
-    progress_ms = 0;
-    duration_ms = 0;
-  }
+    PlaybackState() {
+        title = "N/A";
+        artist = "N/A";
+        album = "N/A";
+        isPlaying = false;
+        volume_percent = 0;
+        progress_ms = 0;
+        duration_ms = 0;
+    }
 };
 
 class SpotifyClient {
-  public:
+   public:
     SpotifyClient();
     ~SpotifyClient();
 
-    void refreshToken(const String& clientID,
-                      const String& clientSecret,
-                      const String& refreshToken);
+    void refreshToken(const String& clientID, const String& clientSecret, const String& refreshToken);
     PlaybackState fetchPlaybackState();
     void play();
     void pause();
@@ -41,7 +39,7 @@ class SpotifyClient {
     void previous();
     void setVolume(int volume);
 
-  private:
+   private:
     HTTPClient httpClient;
 
     String token;
